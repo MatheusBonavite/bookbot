d8041d0581b02867a2738e33e0306be46482eda6
MatheusBonavite <matheusbonavite@gmail.com>
2024-12-22 22:03:32 -0300

    redef is_word

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-def is_upper(line):
-    return line[0] >= 'A' and line[len(line) - 1] <= 'Z'
+def is_upper(character):
+    return character >= 'A' and character <= 'Z'
 
-def is_lower(line):
-    return line[0] >= 'a' and line[len(line) - 1] <= 'z'
-    
+def is_lower(character):
+    return character >= 'a' and character <= 'z'
+
+def is_word(word):
+    if len(word) <= 0:
+        return False
+    if is_upper(word[0])  and is_upper(word[len(word) - 1]):
+        return True
+    if is_upper(word[0])  and is_lower(word[len(word) - 1]):
+        return True
+    if is_lower(word[0])  and is_upper(word[len(word) - 1]):
+        return True
+    if is_lower(word[0])  and is_lower(word[len(word) - 1]):
+        return True
+    return False
+
 def word_counter(line):
     stripped_lines = line.strip().split(" ")
-    is_word = lambda sline : len(sline) > 0 and (is_upper(sline) or is_lower(sline))
     only_words = list(filter(is_word, stripped_lines))
     return len(only_words)
 
