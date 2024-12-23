9b9d2298b6f8c1dd629ad578e5bb4aa0847e9896
MatheusBonavite <matheusbonavite@gmail.com>
2024-12-22 21:50:22 -0300

    add a simple word counter function

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+
+def word_counter(line):
+    stripped_lines = line.strip().split(" ")
+    is_word = lambda sline : len(sline) > 0 and ((sline[0] >= 'A' and sline[len(sline) - 1] <= 'Z') or (sline[0] >= 'a' and sline[len(sline) - 1] <= 'z'))
+    only_words = list(filter(is_word, stripped_lines))
+    return len(only_words)
+
 def main():
+    accum = 0
     with open("books/frankenstein.txt") as file:
-        file_contents = file.read()
-        print(file_contents)
+        for line in file:
+            accum += word_counter(line)
+    print(accum)
+
 
 main()
